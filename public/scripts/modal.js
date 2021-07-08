6461ef88073e3dfd37539c7ecfe60486843c0745
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-07-08 18:30:02 -0300

    Aula 04 - Landing

---
+++ public/scripts/modal.js
@@ -0,0 +1,20 @@
+export default function Modal() {
+  const modalWarapper = document.querySelector(".modal-wrapper");
+  const cancelButton = document.querySelector(".button.cancel");
+
+  cancelButton.addEventListener("click", close);
+
+  function open() {
+    // ADD CLASS ACTIVE
+    modalWarapper.classList.add("active");
+  }
+  function close() {
+    // DELETE CLASS ACTIVE
+    modalWarapper.classList.remove("active");
+  }
+
+  return {
+    open,
+    close,
+  };
+}
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const route = require("./routes");
+const path = require("path");
+
+const server = express();
+
+server.set("view engine", "ejs");
+
+server.use(express.static("public"));
+
+server.set("views", path.join(__dirname, "views"));
+
+// Middleware
+server.use(express.urlencoded({ extended: true }));
+
+server.use(route);
+
+server.listen(3000, () => console.log("🟢 Start Server NodeJS 🟢"));
